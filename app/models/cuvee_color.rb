7e242b956b77e237cd6f88b2bc3d464ce59a5bb3
chloe gregoire <chle.gregire@gmail.com>
2025-04-18 11:36:42 +0200

    modal show on cuvee color index

--- app/models/cuvee_color.rb
+++ app/models/cuvee_color.rb
@@ -3,9 +3,19 @@ class CuveeColor < ApplicationRecord
   belongs_to :color
   has_many :dividende_cuvee_colors, dependent: :destroy
 
+
+
   accepts_nested_attributes_for :color, allow_destroy: true
 
   def self.ransackable_attributes(auth_object = nil)
     ["color_id", "created_at", "cuvee_id", "description", "id", "id_value", "updated_at"]
+  end
+
+  def vineyard
+    cuvee.vinyard_appellation.vinyard
+  end
+
+  def vineyard_appellation
+    cuvee.vinyard_appellation
   end
 end
